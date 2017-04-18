63bca9ea6ccbc4907b160c8e6a5527a56e1ad0bb
Shane Husson <s.husson@garvan.org.au>
2017-04-19 07:41:33 +1000

    only update variants if there is a diff

--- src/app/components/parts/search-results/search-results.component.ts
+++ src/app/components/parts/search-results/search-results.component.ts
@@ -9,6 +9,7 @@ import { AutocompleteResult } from '../../../model/autocomplete-result';
 import { Gene } from '../../../model/gene';
 import { Region } from '../../../model/region';
 import { SearchFilterService } from '../../../services/search-filter.service';
+import { SearchFilterItem, SearchFilterItemSerialised } from '../../../model/search-filter-item';
 
 @Component({
     selector: 'app-search-results',
@@ -19,8 +20,9 @@ import { SearchFilterService } from '../../../services/search-filter.service';
 export class SearchResultsComponent implements OnInit, OnDestroy {
     @Input() autocomplete: AutocompleteResult<any>;
     @Output() errorEvent = new EventEmitter();
-    public variants: Variant[] = [];
-    public loadingVariants = false;
+    variants: Variant[] = [];
+    loadingVariants = false;
+    loadGb = true;
     private subscriptions: Subscription[] = [];
 
     constructor(public searchService: VariantSearchService,
@@ -43,15 +45,31 @@ export class SearchResultsComponent implements OnInit, OnDestroy {
         this.subscriptions.push(this.sfs.updates.subscribe(() => {
             let q = this.searchService.lastQuery;
             let previous = this.searchService.lastQuery.clinicalFilters;
-            q.clinicalFilters = this.sfs.appliedItems.filter((f) => f.isValid() && f.enabled);
-            let diff = () => {
-                let a = q.clinicalFilters;
-                return a.filter((x) => previous.indexOf(x) < 0)
-                    .concat(previous.filter(x => a.indexOf(x) < 0));
+            let validFilters: SearchFilterItem[] = this.sfs.appliedItems.filter((f) => f.isValid() && f.enabled);
 
-            };
-            if (diff().length > 0) {
-                this.searchService.getVariants(q);
+            let newFilters = [];
+            validFilters.forEach((cf) => {
+                let existing = previous.find((ocf: SearchFilterItemSerialised) => cf.isEqual(ocf));
+                if (!existing) {
+                    newFilters.push(cf);
+                }
+            });
+
+            let deletedFilters = [];
+            previous.forEach((ocf: SearchFilterItemSerialised) => {
+                let existing = validFilters.find((cf) => cf.isEqual(ocf));
+                if (!existing) {
+                    deletedFilters.push(ocf);
+                }
+            });
+
+            if (newFilters.length > 0 || deletedFilters.length > 0) {
+                q.clinicalFilters = <SearchFilterItemSerialised[]>(validFilters);
+                this.loadGb = false;
+                this.searchService.getVariants(q).then(() => {
+                    this.loadGb = true;
+                    this.cd.detectChanges();
+                });
             }
         }));
 
