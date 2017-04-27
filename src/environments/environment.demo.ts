f98819b093dfab226bfad4867e9eddc2d12ae14f
Shane Husson <s.husson@garvan.org.au>
2017-04-28 09:13:44 +1000

    support firebase hosting

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    ensemblDomain: 'sgc.garvan.org.au/ensembl',
+    ensemblProtocol: 'https',
+    baseHref: '',
+    auth0ClientId: 'eS2HA6aSYnxCXFvo9bzHpV1DI6H1yw0l',
+    auth0Domain: 'sgc.au.auth0.com',
+    beaconNetworkUrl: 'https://beacon-network.org/api',
+    vsalUrl: 'https://sgc.garvan.org.au:8443/vsal/core/find',
+    elasticUrl: 'https://dr-sgc.kccg.garvan.org.au/_elasticsearch',
+    durlUrl: 'https://wt-ec1ac815dce38c76c2e7662693b82189-0.run.webtask.io/durl-dev',
+    sentryUrl: 'https://90b2013bdfef4fef9491990e6ad379c6@sentry.io/158605',
+    jhubUrl: 'https://vsal.garvan.org.au:7777'
+};
