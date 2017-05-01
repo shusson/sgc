4913918a7a2fda476bf7b6b582bf15ae7b4a48e5
Shane Husson <shane.a.husson@gmail.com>
2017-05-01 16:46:13 +1000

    scroll meta-information subview when there is overflow (#19)
    handle asterix(*) alternate values in the variant view

--- src/app/components/pages/variant/variant.component.ts
+++ src/app/components/pages/variant/variant.component.ts
@@ -66,8 +66,8 @@ export class VariantComponent implements OnInit, OnDestroy {
             let m = r.exec(params['query']);
             let chromo = m[1];
             let start = Number(m[2]);
-            let reference = m[3].replace('*', '');
-            let alternate = m[4].replace('*', '');
+            let reference = m[3];
+            let alternate = m[4];
 
             let sq = new SearchQuery(chromo, start, start, [new SearchOption('', 'returnAnnotations', [], 'true')]);
             this.getVariant(sq, reference, alternate);
@@ -113,7 +113,7 @@ export class VariantComponent implements OnInit, OnDestroy {
                 this.error = 'Found more than one variant for query';
             } else if (vf.length > 0) {
                 this.variant = vf[0];
-                if (this.variant.alternate.length !== 1) {
+                if (this.variant.alternate.length !== 1 || this.variant.alternate === '*') {
                     this.beaconSupported = false;
                 } else {
                     this.beacons = this.bss.searchBeacon(this.beaconQuery(this.variant));
