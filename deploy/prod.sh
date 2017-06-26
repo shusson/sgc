7bc402390d51d4eaa74afb5be136632eeb3a448c
Shane Husson <shane.a.husson@gmail.com>
2017-06-26 19:41:19 +1000

    add non interactive mode to firebase deploys on travis (#28)

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -3,6 +3,6 @@
 set -ev
 
 ng build --target=production --environment=prod --aot;
-firebase deploy --project=prod --token "$FIREBASE_TOKEN"
+firebase deploy --project=prod --token "$FIREBASE_TOKEN" --non-interactive
 
 exit 0;
