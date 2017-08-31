5a1dcef8f111cf72e7b65bb2116fe186119cd10e
Shane Husson <shane.a.husson@gmail.com>
2017-08-31 11:04:39 +1000

    update auth

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,50 +1,43 @@
-import { Injectable, Optional, Inject } from '@angular/core';
-import { tokenNotExpired } from 'angular2-jwt';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { constants } from '../app.constants';
 import Auth0Lock from 'auth0-lock';
 import { MdDialog } from '@angular/material';
 import { ErrorDialogComponent } from '../components/parts/error-dialog/error-dialog.component';
+import * as jwtDecode from 'jwt-decode';
 
-const authKey = 'id_token';
-const cannyKey = 'canny_token';
-
-const options: any = {
-    theme: {
-        logo: constants.GARVAN_KCCG_LOGO,
-        primaryColor: constants.PRIMARY_COLOR,
-    },
-    languageDictionary: {
-        title: ''
-    },
-    auth: {
-        redirectUrl: constants.ORIGIN_URL + 'initiatives',
-        responseType: 'id_token token'
-    },
-    avatar: null
-};
+export const expiredAtKey = 'expired_at';
+export const cannyKey = 'canny_token';
 
 @Injectable()
 export class Auth {
+    lock = new Auth0Lock(environment.auth0ClientId, environment.auth0Domain, {
+        theme: {
+            logo: constants.GARVAN_KCCG_LOGO,
+            primaryColor: constants.PRIMARY_COLOR,
+        },
+        languageDictionary: {
+            title: ''
+        },
+        avatar: null,
+        autoclose: true,
+        auth: {
+            redirectUrl: constants.ORIGIN_URL + 'initiatives',
+            responseType: 'token id_token',
+            params: {
+                scope: 'openid email'
+            }
+        }
+    });
 
     constructor(private router: Router,
-                public dialog: MdDialog,
-                @Inject('NULL_VALUE') @Optional() private tokenExpiredFn?: any,
-                @Inject('NULL_VALUE') @Optional() private location?: any,
-                @Inject('NULL_VALUE') @Optional() public lock?: any) {
-        this.tokenExpiredFn = tokenExpiredFn ? tokenExpiredFn : tokenNotExpired;
-        this.lock = lock ? lock : new Auth0Lock(environment.auth0ClientId, environment.auth0Domain, options);
-        this.location = location ? location : window.location;
+                public dialog: MdDialog) {
 
         this.lock.on('authenticated', (authResult: any) => {
             try {
                 if (authResult.idToken && authResult.idToken !== 'undefined') {
-                    localStorage.setItem(authKey, authResult.idToken);
-                    localStorage.setItem(cannyKey, authResult.idTokenPayload['http://sgc/cannyToken']);
-                    window.setTimeout(() => {
-                        this.router.navigateByUrl(decodeURIComponent(authResult.state));
-                    }, 100);
+                    this.setSession(authResult);
                 }
             } catch (e) {
                 window.setTimeout(() => {
@@ -57,8 +50,14 @@ export class Auth {
         });
 
         this.lock.on('unrecoverable_error', (authResult: any) => {
-            localStorage.removeItem(authKey);
+            localStorage.removeItem(expiredAtKey);
             localStorage.removeItem(cannyKey);
+            window.setTimeout(() => {
+                this.dialog.open(
+                    ErrorDialogComponent,
+                    { data: "An error occurred while trying to authenticate. Please ensure private browsing is disabled and try again."}
+                );
+            }, 100);
         });
 
     }
@@ -88,14 +87,29 @@ export class Auth {
     }
 
     public authenticated() {
-        return this.tokenExpiredFn(authKey);
+        const exp = localStorage.getItem(expiredAtKey);
+        if (!exp) {
+            return false;
+        }
+        const expiresAt = JSON.parse(localStorage.getItem(expiredAtKey));
+        return new Date().getTime() < expiresAt;
     };
 
     public logout() {
-        localStorage.removeItem(authKey);
+        localStorage.removeItem(expiredAtKey);
         localStorage.removeItem(cannyKey);
-        this.location.href = `https://${ environment.auth0Domain }/v2/logout?returnTo=${ constants.ORIGIN_URL }`;
+        window.location.href = `https://${ environment.auth0Domain }/v2/logout?returnTo=${ constants.ORIGIN_URL }`;
     };
+
+    private setSession(authResult): void {
+        const idToken = jwtDecode(authResult.idToken);
+        localStorage.setItem(cannyKey, idToken['http://sgc/cannyToken']);
+        const expiresAt = JSON.stringify(idToken.exp * 1000);
+        localStorage.setItem(expiredAtKey, expiresAt);
+        window.setTimeout(() => {
+            this.router.navigateByUrl(decodeURIComponent(authResult.state));
+        }, 100);
+    }
 
     cannyToken(): string {
         return localStorage.getItem(cannyKey);
