4f2eaad4b05c727b7ca27a8095adbdf1b6a85a84
Shane Husson <shane.a.husson@gmail.com>
2017-11-20 13:45:04 +1100

    improve handling of error messages from services

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,5 +3,7 @@ import { environment } from '../environments/environment';
 export const constants = {
     GARVAN_KCCG_LOGO: 'assets/logos/kccggarvan.png',
     PRIMARY_COLOR: '#003263',
-    ORIGIN_URL: location.origin + '/' + environment.baseHref
+    ORIGIN_URL: location.origin + '/' + environment.baseHref,
+    GENERIC_ERROR_MESSAGE: 'There was an internal error and our team has been notified. ' +
+    'In the meantime please try again or contact us at sgc@garvan.org.au'
 };
