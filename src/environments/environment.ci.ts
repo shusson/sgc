a05633bd2fe34cef339b6301b01ce93867250f54
Shane Husson <shane.a.husson@gmail.com>
2017-11-21 12:18:06 +1100

    update vsql domain

--- src/environments/environment.ci.ts
+++ src/environments/environment.ci.ts
@@ -7,7 +7,7 @@ export const environment = {
     auth0ClientId: 'eS2HA6aSYnxCXFvo9bzHpV1DI6H1yw0l',
     auth0Domain: 'sgc.au.auth0.com',
     beaconNetworkUrl: 'https://beacon-network.org/api',
-    vsalUrl: 'https://services.vectis-api.com:80/variants',
+    vsalUrl: 'https://vsql.vectis-api.com:443/variants',
     elasticUrl: 'https://dr-sgc.kccg.garvan.org.au/_elasticsearch',
     durlUrl: 'https://wt-ec1ac815dce38c76c2e7662693b82189-0.run.webtask.io/durl-dev',
     sentryUrl: 'https://90b2013bdfef4fef9491990e6ad379c6@sentry.io/158605',
