7eee677285d36329d98f7c56324a20aea0c577a4
Shane Husson <shane.a.husson@gmail.com>
2017-11-23 12:11:10 +1100

    ensure position search is always sane

--- src/app/model/position.ts
+++ src/app/model/position.ts
@@ -1,9 +1,8 @@
 import { Region } from './region';
 
 export class Position extends Region {
-    constructor(c: string,
-                public position: number) {
-        super(c, position - 50, position + 50);
+    constructor(c: string, public position: number) {
+        super(c, position <= 50 ? 0 : position - 50, position <= 50 ? 50 : position + 50);
     }
 
     name() {
