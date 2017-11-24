f1b587a6ca4c774547d9b580b60542850e5a6c58
Shane Husson <shane.a.husson@gmail.com>
2017-11-24 20:30:52 +1100

    update error message (#96)

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -4,6 +4,6 @@ export const constants = {
     GARVAN_KCCG_LOGO: 'assets/logos/kccggarvan.png',
     PRIMARY_COLOR: '#003263',
     ORIGIN_URL: location.origin + '/' + environment.baseHref,
-    GENERIC_ERROR_MESSAGE: 'There was an internal error and our team has been notified. ' +
-    'In the meantime please try again or contact us at sgc@garvan.org.au'
+    GENERIC_SERVICE_ERROR_MESSAGE: 'There was an error connecting to one of our services.' +
+    ' Please check your network and please try again or contact us at sgc@garvan.org.au'
 };
