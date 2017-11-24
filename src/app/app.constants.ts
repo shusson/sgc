7fd13cbb403241d8dc6d64ee9298c3b05d5928f9
Shane Husson <shane.a.husson@gmail.com>
2017-11-24 20:43:08 +1100

    fix error message

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -5,5 +5,5 @@ export const constants = {
     PRIMARY_COLOR: '#003263',
     ORIGIN_URL: location.origin + '/' + environment.baseHref,
     GENERIC_SERVICE_ERROR_MESSAGE: 'There was an error connecting to one of our services.' +
-    ' Please check your network and please try again or contact us at sgc@garvan.org.au'
+    ' Please check your network and try again or contact us at sgc@garvan.org.au'
 };
